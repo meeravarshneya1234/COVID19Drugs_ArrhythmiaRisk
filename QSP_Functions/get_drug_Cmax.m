9b43983eed76a0b75929bb02bf8fcb514786a8b4
meeravarshneya1234 <meera.varshneya@icahn.mssm.edu>
2020-10-28 22:16:38 -0400

    remove old files

--- QSP_Functions/get_drug_Cmax.m
+++
@@ -1,8 +0,0 @@
-function drug_Cmax = get_drug_Cmax(drug)
-
-[data,labels] = xlsread('Crumb_IC50.xlsx');
-drug_names = labels(2:end,1);
-col_names = labels(1,2:end);
-d_index = ismember(drug_names,drug);
-c_index = strcmp('Free Cmax',col_names);
-drug_Cmax = data(d_index,c_index);
